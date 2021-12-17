3fdfdc63976e94dbfcca46057ad87c9223390fdb
boxstory <83285016+boxstory@users.noreply.github.com>
2021-12-17 13:16:38 +0300

    deployformupdate

--- ezzydelivery/webpages/forms.py
+++ ezzydelivery/webpages/forms.py
@@ -2,16 +2,16 @@ from django import forms
 
 
 class ContactForm(forms.Form):
+    DELIVERY_REQUEST = 'dr'
+    FULLFILLMENT = 'fl'
     DRIVER_JOB = 'd'
     FEEDBACK = 'fb'
-    NEW_FEATURE = 'nf'
-    DELIVERY_REQUEST = 'dr'
     OTHER = 'o'
     purpose_choices = (
-        (FEEDBACK, 'Feedback'),
-        (NEW_FEATURE, 'Feature Request'),
         (DELIVERY_REQUEST, 'Delivery Request'),
+        (FULLFILLMENT, 'Fullfillment Request'),
         (DRIVER_JOB, 'Driver Jobs'),
+        (FEEDBACK, 'Feedback'),
         (OTHER, 'Other'),
     )
 
@@ -21,3 +21,5 @@ class ContactForm(forms.Form):
     purpose = forms.ChoiceField(choices=purpose_choices)
     message = forms.CharField(
         widget=forms.Textarea(attrs={'cols': 40, 'rows': 5}))
+
+
