5b1aabc0ff350a8b23a452fc87ccd19f5963cad7
boxstory <boxstory.clt@gmail.com>
2022-02-25 16:39:02 +0300

    core change

--- ezzydelivery/core/urls.py
+++ ezzydelivery/core/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from core import views as core_views
 from webpages import views as webpages_views
 from delivery import views as delivery_views
 from orders import views as orders_views
 
 app_name = 'core'
 urlpatterns = [
+    path('profile/', core_views.profile, name='profile'),
 
 
 
