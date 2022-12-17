ea5df3e0095a1d6bbf1f8fdf168dd57b6f9a47c1
boxstory <boxstory.clt@gmail.com>
2022-12-17 12:24:35 +0300

    joiin drivers update

--- ezzydelivery/ezzydelivery/settings.py
+++ ezzydelivery/ezzydelivery/settings.py
@@ -17,8 +17,8 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = ['ezzydelivery.qa', 'www.ezzydelivery.qa',
-]
+ALLOWED_HOSTS = ['ezzydelivery.qa',
+                 'www.ezzydelivery.qa', '127.0.0.1', 'localhost']
 
 
 # Application definition
