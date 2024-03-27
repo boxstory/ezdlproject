f89194d0b329c738b7da71bfdf2820711e88590d
boxstory <boxstory.clt@gmail.com>
2024-03-27 20:59:45 +0300

    update apil

--- ezzydelivery/ezzy_api/views.py
+++ ezzydelivery/ezzy_api/views.py
@@ -1,7 +1,4 @@
 import requests
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
 from rest_framework import permissions
 from rest_framework import generics
 from decouple import config
